9c34bb6ba58bfb2f0119e19d3f4661594518e387
capnemo20 <z.sujon20@outlook.com>
2024-10-07 22:26:16 +0100

    30-8 Keys, values, entries, delete, seal, freeze

---
+++ js-recap-basic-es6/keys.js
@@ -0,0 +1,27 @@
+const glass = { name: "glass", color: "golden", price: 12, isCleaned: true };
+// console.log(glass);
+const keys = Object.keys(glass);
+// console.log(keys);
+//[ 'name', 'color', 'price', 'isCleaned' ]
+const pair = Object.entries(glass);
+console.log(pair);
+//array of array
+// [
+//   [ 'name', 'glass' ],
+//   [ 'color', 'golden' ],
+//   [ 'price', 12 ],
+//   [ 'isCleaned', true ]
+// ]
+
+//delete property
+delete glass.isCleaned;
+console.log(glass);
+
+//bhejailla way to delete properties
+const {isCleaned, ...shortGlass} = glass;
+console.log(glass);
+
+//freeze
+Object.freeze(glass);
+glass.source = "Bangladesh";
+console.log(glass);
